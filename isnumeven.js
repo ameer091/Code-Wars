255ae28d663b721abbceaa3391cb8c7b8a7c5baf
Ameer Fields <ameersf0@gmail.com>
2023-05-23 12:49:44 -0400

    Is the argument placed in the function even?

---
+++ isnumeven.js
@@ -0,0 +1,38 @@
+/*
+Is it even?
+
+DESCRIPTION:
+In this Kata we are passing a number (n) into a function.
+
+Your code will determine if the number passed is even (or not).
+
+The function needs to return either a true or false.
+
+Numbers may be positive or negative, integers or floats.
+
+Floats with decimal part non equal to zero are considered UNeven for this kata.
+
+*/
+
+function isEvenOrNot(n) {
+  if (Number.isInteger(n)) {
+    return n % 2 === 0;
+  } else {
+    return false;
+  }
+}
+
+//Other Solutions
+function testEven(n) {
+  return n%2===0;
+}
+
+//Other Solutions
+function testEven(n) {
+  return n % 2 === 0 ? true : false;
+}
+
+//Other Solutions
+function testEven(n){
+  return !(n % 2);
+}

967fe23acac8152fcbd5763227abd788503f9633
Ameer Fields <ameersf0@gmail.com>
2023-06-17 21:20:35 -0400

    Square a number argument

---
+++ squaredinput.js
@@ -0,0 +1,13 @@
+/*
+Now you have to write a function that takes an argument and returns the square of it.
+*/
+
+const square = (num) => num**2;
+
+//Other Solutions
+const square = (n) => n * n;
+
+//Other Solutions
+var square = function(a){
+  return a*a;
+}
